aa41180d3642c8e1e99ba9b6135ac09f8be95d2c
Olha Busheva <OlgaUA841@gmail.com>
2024-02-23 13:04:29 +0100

    calculator supports division

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ def substract(x, y):
     return x - y
 
 
+def division(x, y):
+    return x / y
+
+
 num1 = int(input('Please enter first number: '))
 
 num2 = int(input('Please enter second number: '))
 
 print(f'{num1} + {num2} = {add(num1, num2)}')
 print(f'{num1} - {num2} = {substract(num1, num2)}')
+print(f'{num1} / {num2} = {division(num1, num2)}')
